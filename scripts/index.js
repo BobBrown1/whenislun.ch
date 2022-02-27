491dfa5d84fbbaa9d64cf156793c9b02b9edba0f
Bob Brown <bob-brown@Ronens-MBP.hsd1.ca.comcast.net>
2022-02-26 19:21:31 -0800

    adding help modal

--- scripts/index.js
+++ scripts/index.js
@@ -100,15 +100,21 @@ function shareSched() {
 }
 }
 
+function showHelp() {
+      document.getElementById("helpModal").style.display = "block";
+}
+
+
 // When the user clicks on <span> (x), close the modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+function closeModal(modal) {
+  document.getElementById(modal).style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-if (event.target == document.getElementById("myModal")) {
+if (event.target == document.getElementById("myModal") || event.target == document.getElementById("helpModal")) {
   document.getElementById("myModal").style.display = "none";
+  document.getElementById("helpModal").style.display = "none";
 }
 }
 
