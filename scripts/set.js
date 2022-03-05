88e2831f49855fc7ebc4596a2f407c7a06b4ce76
Bob Brown <bob-brown@Ronens-MBP.hsd1.ca.comcast.net>
2022-03-04 21:44:45 -0800

    updating share feature

--- scripts/set.js
+++ scripts/set.js
@@ -10,9 +10,38 @@ if (urlParams.has('s')) {
     var schedList = []
     for (var i = 0; i < classList.length; i++) {
         var itemList = classList[i].split("=");
+        if(checkAll(itemList[0], itemList[1])) {
         schedList.push({"name": itemList[0], "time": itemList[1]})
+        } else {
+            window.location.href = `https://${window.location.hostname}/`;
+        }
     }
     localStorage.setItem("classes", JSON.stringify(schedList));
 } else {
     window.location.href = `https://${window.location.hostname}/`;
+}
+
+function checkAll(className, perTime) {
+      if (className.length < 1) {
+        continue;
+      } else {
+          alert("Invalid class name(s) in link.");
+          return false;
+      }
+
+      var timereg=new RegExp("^([01]?[0-9]|2[0-3]):[0-5][0-9] ?[aApP]\.?[mM]\.?$");
+      var nomilreg=new RegExp("^(1[3-9]|2[0-3]):[0-5][0-9] ?[aA]\.?[mM]\.?$");
+
+      if (!timereg.test(perTime)) {
+        alert("Invalid time format in class: Period " + (i + 1));
+        return false;
+      };
+      if (nomilreg.test(perTime)) {
+        alert("You used \"AM\" with a military time between 13:00 and 23:59 in class: Period " + (i + 1));
+        return false;
+      };
+
+      if (!nomilreg.test(perTime) && timereg.test(perTime)) {
+          return true;
+      };
 }
